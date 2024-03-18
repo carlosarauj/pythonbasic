1d7cdd802c95585bee0acdf3fdbf538ea4e520bc
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-03-18 19:24:28 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,11 @@
+saldo = 100
+
+if saldo >= 0:
+    print(f"Saldo positivo: {saldo}")
+else:
+    print(f"Saldo negativo: {saldo}")
+
+nome = "Carlos"
+idade = 21
+
+print(f"meu nome é {nome}, tenho {idade} anos e meu saldo é {saldo}")
